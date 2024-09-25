58c255e9f5f28dcc78186d4e9e9651112bf5f20f
brianasanchez <a.23308051281213@cbtis128.edu.mx>
2024-09-24 20:02:27 -0600

    Add files via upload

---
+++ numero.py
@@ -0,0 +1,15 @@
+print(" ") # print imprime un espacio
+
+print("Briana Sarahi Sanchez Perez 3-W")
+print(" ") # print imprime un espacio
+
+a = 7  # dando valor a la variable "A"
+
+numero = input("Ingresa un numero:") # te pide que ingreses un numero 
+
+if (a == numero): # a es igual a numero
+    print("El numero es el correcto") # si el numero es 7 se imprime 
+else: # ejecuta ambas partes falsa o verdadera
+    print("El numero es incorrecto") # si el numero no es 7 se imprime
+    
+print(" ") # print imprime un espacio
